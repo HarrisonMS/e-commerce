47355b63dc6f1a8e794db20a0f9c4f7764ad9654
Harrison <hmseaborn@gmail.com>
2020-12-03 09:22:56 -0800

    wip

--- ecomm/src/App.js
+++ ecomm/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
-import HomePage from "./components/homePage";
+import HomePage from "./pages/homepage/homePage.jsx";
+import ShopPage from "./pages/shop/shopPage.jsx";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
