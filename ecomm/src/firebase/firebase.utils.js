833f8932a02e3e8e1e9111081f62aa2eaaead766
Harrison <hmseaborn@gmail.com>
2020-12-15 15:06:45 -0800

    wip

--- ecomm/src/firebase/firebase.utils.js
+++ ecomm/src/firebase/firebase.utils.js
@@ -13,12 +13,22 @@ const config = {
   measurementId: "G-LF19N0RJBP",
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // used to add collections into firebase
+  // const collectionRef = firestore.collection("users");
 
   const snapShot = await userRef.get();
+  // used to add collections into firebase
+  // const collectionSnapShot = await collectionRef.get();
+  // console.log("collection", {
+  //   collection: collectionSnapShot.docs.map((doc) => doc.data()),
+  // });
+
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -37,7 +47,57 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-firebase.initializeApp(config);
+// used to add collections into firebase
+
+// set add collection in else block on componentDidMount
+// addCollectionAndDocuments(
+//   "collections",
+//   collections.map(({ title, items }) => ({ title, items }))
+// );
+// import { selectCollectionsForPreview } from "./redux/shop/shop.selectors";
+// import {
+//   auth,
+//   createUserProfileDocument,
+//   addCollectionAndDocuments,
+// } from "./firebase/firebase.utils";
+
+// const mapStateToProps = createStructuredSelector({
+//   currentUser: selectCurrentUser,
+//   collections: selectCollectionsForPreview,
+// });
+
+// code used in app.js to pull collections with shop selector and looping through to add correct items
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((object) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, object);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+  return transformedCollections.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
